2c6bae45facc5e41e3f9fb475ed35b5f7a720b3f
Alexander Weiher <aweiher@users.noreply.github.com>
2016-04-16 14:59:59 +0200

    remove SLACK_TOKEN requirement

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var Botkit = require('botkit')
 
 // Expect a SLACK_TOKEN environment variable
-var slackToken = process.env.SLACK_TOKEN
-if (!slackToken) {
-  console.error('SLACK_TOKEN is required!')
-  process.exit(1)
-}
+//var slackToken = process.env.SLACK_TOKEN
+//if (!slackToken) {
+//  console.error('SLACK_TOKEN is required!')
+//  process.exit(1)
+//}
 
 var controller = Botkit.slackbot()
 
