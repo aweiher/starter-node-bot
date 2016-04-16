c1ef7b01bbf5be986952aaca3947be025b7ed2c2
Alexander Weiher <aweiher@users.noreply.github.com>
2016-04-16 16:30:16 +0200

    fix: issue with bot

--- questions.js
+++ questions.js
@@ -1,68 +1,70 @@
+exports.init = function (bot) {
 
-var askStart = function( response, convo) {
+
+  var askStart = function( response, convo) {
     convo.ask('Are you interested in some amazing project opportunities?', [
         {
-            pattern: bot.utterances.yes,
-            callback: function(response,convo) {
-                convo.say('Great!');
-                //askSkills(response, convo);
-                convo.next();
-            }
+          pattern: bot.utterances.yes,
+          callback: function(response,convo) {
+            convo.say('Great!');
+            //askSkills(response, convo);
+            convo.next();
+          }
         }/*,
-        {
-            pattern: bot.utterances.no,
-            callback: function(response,convo) {
-                convo.say('No Problem - just write me as soon you are interested.');
-                // do something else...
-                convo.next();
-            }
-        },
-        {
-            default: true,
-            callback: function(response,convo) {
-                convo.say('Sorry, I dont understand you .. Please answer with yes or no.');
-                convo.repeat();
-                convo.next();
-            }
-        }*/
-    ]
+         {
+         pattern: bot.utterances.no,
+         callback: function(response,convo) {
+         convo.say('No Problem - just write me as soon you are interested.');
+         // do something else...
+         convo.next();
+         }
+         },
+         {
+         default: true,
+         callback: function(response,convo) {
+         convo.say('Sorry, I dont understand you .. Please answer with yes or no.');
+         convo.repeat();
+         convo.next();
+         }
+         }*/
+      ]
     );
-};
+  };
 
-var askSkills = function( response, convo) {
+  var askSkills = function( response, convo) {
     convo.ask('Which of the following skills do you have? (*backend*, *frontend*, *api*)', [
-        {
-            pattern: 'backend',
-            callback: function (response, convo) {
-                convo.say('Perfect!');
-                convo.next();
-            }
-        }, {
-            pattern: 'backend',
-            callback: function (response, convo) {
-                convo.say('Perfect!');
-                convo.next();
-            }
-        }, {
-            pattern: 'backend',
-            callback: function (response, convo) {
-                convo.say('Perfect!');
-                convo.next();
-            }
-        }, {
-            default: true,
-            callback: function(response,convo) {
-                convo.say('I did not understand which projects you search.');
-                convo.repeat();
-                convo.next();
-            }
+      {
+        pattern: 'backend',
+        callback: function (response, convo) {
+          convo.say('Perfect!');
+          convo.next();
         }
+      }, {
+        pattern: 'backend',
+        callback: function (response, convo) {
+          convo.say('Perfect!');
+          convo.next();
+        }
+      }, {
+        pattern: 'backend',
+        callback: function (response, convo) {
+          convo.say('Perfect!');
+          convo.next();
+        }
+      }, {
+        default: true,
+        callback: function(response,convo) {
+          convo.say('I did not understand which projects you search.');
+          convo.repeat();
+          convo.next();
+        }
+      }
     ]);
-};
+  };
 
-exports.init = function(bot) {
-   return {
-       askStart: askStart,
-       askSkills: askSkills
-   };
+
+  return {
+    askStart: askStart,
+    askSkills: askSkills
+  };
 };
