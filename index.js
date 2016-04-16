aff5272b44fe2448f3ce9e3616fd61d1bc240b62
Alexander Weiher <aweiher@users.noreply.github.com>
2016-04-16 23:50:08 +0200

    add experimental webserver example

--- index.js
+++ index.js
@@ -89,7 +89,53 @@ controller.hears(['debug_info'], ['direct_message'], function (bot, message) {
 });
 
 
-// global fallback
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+controller.setupWebserver(process.env.PORT,function(err,webserver) {
+
+  webserver.get('/',function(req,res) {
+
+    var html = '<h1>Super Insecure Form</h1><p>Put text below and hit send - it will be sent to every team who has added your integration.</p><form method="post" action="/unsafe_endpoint"><input type="text" name="text" /><input type="submit"/></form>';
+    res.send(html);
+
+  });
+
+  // This is a completely insecure form which would enable
+  // anyone on the internet who found your node app to
+  // broadcast to all teams who have added your integration.
+  // it is included for demonstration purposes only!!!
+  webserver.post('/unsafe_endpoint',function(req,res) {
+    var text = req.body.text;
+    text = text.trim();
+
+    controller.storage.teams.all(function(err,teams) {
+      var count = 0;
+      for (var t in teams) {
+        if (teams[t].incoming_webhook) {
+          count++;
+          controller.spawn(teams[t]).sendWebhook({
+            text: text
+          },function(err) {
+            if(err) {
+              console.log(err);
+            }
+          });
+        }
+      }
+
+      res.send('Message sent to ' + count + ' teams!');
+    });
+  });
+
+  controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
+    if (err) {
+      res.status(500).send('ERROR: ' + err);
+    } else {
+      res.send('Success!');
+    }
+  });
 });
+
+
+// global fallback
+//controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
+//  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+//});
