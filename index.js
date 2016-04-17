c7378aefbdfafea79196aed5b74085bd122ac0b3
Alexander Weiher <aweiher@users.noreply.github.com>
2016-04-17 08:59:19 +0200

    remove webserver code

--- index.js
+++ index.js
@@ -27,12 +27,6 @@ controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 });
 
-controller.configureSlackApp({
-  clientId: bot.config.clientId,
-  clientSecret: bot.config.clientSecret,
-  scopes: ['incoming-webhook']
-});
-
 controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
   bot.reply(message, 'I send you a message.');
 
@@ -57,7 +51,7 @@ controller.hears(['add'], ['direct_message'], function (bot, message) {
 
 controller.hears('.*', ['mention'], function (bot, message) {
   bot.reply(message, 'I heard you like projects? :heart:')
-})
+});
 
 controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
   var help = 'I will respond to the following messages: \n' +
@@ -78,14 +72,14 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
     title_link: 'https://beepboophq.com/',
     text: text,
     color: '#7CD197'
-  }]
+  }];
 
   bot.reply(message, {
     attachments: attachments
   }, function (err, resp) {
     console.log(err, resp)
   })
-})
+});
 
 
 // debug
@@ -93,53 +87,6 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
 controller.hears(['debug_info'], ['direct_message'], function (bot, message) {
   bot.reply(message, "ENV: " + JSON.stringify(bot.config))
 });
-
-
-controller.setupWebserver(process.env.PORT,function(err,webserver) {
-
-  webserver.get('/',function(req,res) {
-
-    var html = '<h1>Super Insecure Form</h1><p>Put text below and hit send - it will be sent to every team who has added your integration.</p><form method="post" action="/unsafe_endpoint"><input type="text" name="text" /><input type="submit"/></form>';
-    res.send(html);
-
-  });
-
-  // This is a completely insecure form which would enable
-  // anyone on the internet who found your node app to
-  // broadcast to all teams who have added your integration.
-  // it is included for demonstration purposes only!!!
-  webserver.post('/unsafe_endpoint',function(req,res) {
-    var text = req.body.text;
-    text = text.trim();
-
-    controller.storage.teams.all(function(err,teams) {
-      var count = 0;
-      for (var t in teams) {
-        if (teams[t].incoming_webhook) {
-          count++;
-          controller.spawn(teams[t]).sendWebhook({
-            text: text
-          },function(err) {
-            if(err) {
-              console.log(err);
-            }
-          });
-        }
-      }
-
-      res.send('Message sent to ' + count + ' teams!');
-    });
-  });
-
-  controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
-    if (err) {
-      res.status(500).send('ERROR: ' + err);
-    } else {
-      res.send('Success!');
-    }
-  });
-});
-
 
 // global fallback
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
